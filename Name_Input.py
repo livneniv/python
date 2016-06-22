27862d989288b00b735ad7a823ce9ab430a67a56
Livne Niv <livneniv@gmail.com>
2016-06-22 14:12:15 +0300

    added files

---
+++ Name_Input.py
@@ -0,0 +1,8 @@
+x=10
+name=raw_input('Whats your name?' )
+age=raw_input('And can you tell me your age?' )
+print 'Hello ',name
+if age>x
+ print 'I have a cousin who is younger than you'
+else
+	print 'than you can play with my cousin, he is about your age'
